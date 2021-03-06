d364b40f757f89d5f395ed1f9e323f5dbbb1bd94
Volodymyr Smolianinov <Voidozzer@users.noreply.github.com>
2021-03-06 09:40:42 +0100

    Update to Flutter 2 (#5)
    * Update to Flutter 2
    
    * Fixes after PR
    
    Co-authored-by: Alex Demchenko <alexdemchenko@yahoo.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 2.0.0
+
+- Update to Flutter 2
+
 ## 1.1.1
 
 - Finish documentation
--- lib/src/util.dart
+++ lib/src/util.dart
@@ -2,7 +2,7 @@ import 'message.dart' show Status;
 
 /// Converts [stringStatus] to the [Status] enum.
 Status getStatusFromString(String stringStatus) {
-  for (Status status in Status.values) {
+  for (final status in Status.values) {
     if (status.toString() == 'Status.$stringStatus') {
       return status;
     }
