c50b43d07c7a8eb6d5b78498ce03fb33c06addae
shubhambsps <73817129+shubhambsps@users.noreply.github.com>
2021-09-03 12:18:24 +0530

    Update flutter_chat_types.dart

--- lib/flutter_chat_types.dart
+++ lib/flutter_chat_types.dart
@@ -4,6 +4,7 @@ export 'src/message.dart';
 export 'src/messages/custom_message.dart';
 export 'src/messages/file_message.dart';
 export 'src/messages/image_message.dart';
+export 'src/messages/partial_file.dart';
 export 'src/messages/partial_image.dart';
 export 'src/messages/partial_text.dart';
 export 'src/messages/partial_voice.dart';
